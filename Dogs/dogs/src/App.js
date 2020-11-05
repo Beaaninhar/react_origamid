ba30baabd6e3c4bdedf2b1185067b0ee49cc400e
Ana Beatriz <ana.silva@devninjas.com.br>
2020-11-05 08:29:15 -0300

    Arrays

--- Dogs/dogs/src/App.js
+++ Dogs/dogs/src/App.js
@@ -1,49 +1,21 @@
+/* eslint-disable array-callback-return */
 import React from "react";
 
 const App = () => {
-
-  const luana = {
-    cliente: 'Luana',
-    idade: 27,
-    compras: [
-      { nome: 'Notebook', preco: 'R$ 2500' },
-      { nome: 'Geladeira', preco: 'R$ 3000' },
-      { nome: 'Smartphone', preco: 'R$ 1500' },
-    ],
-    ativa: true,
-  };
-  
-  const mario = {
-    cliente: 'Mario',
-    idade: 31,
-    compras: [
-      { nome: 'Notebook', preco: 'R$ 2500' },
-      { nome: 'Geladeira', preco: 'R$ 3000' },
-      { nome: 'Smartphone', preco: 'R$ 1500' },
-      { nome: 'Guitarra', preco: 'R$ 3500' },
-    ],
-    ativa: false,
-  };
-
-  const dados = mario;
-
-  const total = dados.compras.map((item) => Number(item.preco.replace('R$ ', ""))).reduce((a, b) => a + b)
-  console.log(total)
+  const livros = [
+    { nome: 'A Game of Thrones', ano: 1996 },
+    { nome: 'A Clash of Kings', ano: 1998 },
+    { nome: 'A Storm of Swords', ano: 2000 },
+  ];
 
   return (
-    <div>
-      <p>{dados.cliente}</p>
-      <p>{dados.idade}</p>
-      <p>
-        Situação:{' '}
-        <span style={{ color: dados.ativa ? 'green' : 'red' }}>
-          {dados.ativa ? 'Ativa' : 'Inativa'}
-        </span>
-      </p>
-      <p>Total: R$ {total}</p>
-      {total > 10000 && <p>Você está gastando muito</p>}
 
-    </div>
+    <ul>
+      {livros.filter(({ ano }) => ano >= 1998).map(({nome, ano}) => (
+        <li key={nome}>{nome}, {ano}</li>
+      ))}
+    </ul>
+    
   );
 };
 
