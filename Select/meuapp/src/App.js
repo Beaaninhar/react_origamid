13c5a239fc6cbb562a5e3cc1f0fb190a790580be
Ana Beatriz <ana.silva@devninjas.com.br>
2020-12-09 14:18:03 -0300

    Select

--- Select/meuapp/src/App.js
+++ Select/meuapp/src/App.js
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, { useState } from "react";
 
 function App() {
-  return <div>oi</div>;
+  const [select, setSelect] = useState("");
+  return (
+    <form>
+      <select
+        value={select}
+        onChange={({ target }) => setSelect(target.value)}
+        id="produtos"
+      >
+        <option value="" disabled>
+          Selecione
+        </option>
+        <option value="notebook">Notebook</option>
+        <option value="smartphone">Smartphone</option>
+        <option value="tablet">Tablet</option>
+      </select>
+      <p>{select}</p>
+    </form>
+  );
 }
 
 export default App;
